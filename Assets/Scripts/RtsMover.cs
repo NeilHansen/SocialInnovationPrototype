2534fee17673098b6589832011fb6edba03a5c8c
Neil Hansen <neilmickneilh12@hotmail.com>
2018-12-18 19:39:23 -0500

    added projects

---
+++ Assets/Scripts/RtsMover.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RtsMover : MonoBehaviour {
+    public GameObject Unit1;
+    public GameObject Unit2;
+    public GameObject ActiveUnit;
+
+    Vector3 originalPosition;
+
+
+    public LayerMask mask;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+		
+	}
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(new Vector3(originalPosition.x, 0, originalPosition.z), 1.0f);
+    }
+
+    private void OnMouseDown()
+    {
+        if (ActiveUnit != null)
+        {
+            Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, mask))
+            {
+                if (hit.collider.gameObject.tag == "Floor")
+                {
+                    originalPosition = hit.point;
+                }
+            }
+            
+            //originalPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            MovePlayer(new Vector3(originalPosition.x, 0,originalPosition.z));
+        }
+    }
+
+    void MovePlayer(Vector3 newPos)
+    {
+        ActiveUnit.GetComponent<NavMeshAgent>().SetDestination(newPos);
+    }
+
+    
+
+    
+}
---
+++ Assets/Scripts/UnitHighlight.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitHighlight : MonoBehaviour {
+
+    public RtsMover rtsMover;
+
+    public Material Green;
+    public Material Red;
+    public bool isClicked = false;
+    public bool isHovered = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if(isClicked || isHovered)
+        {
+            this.gameObject.GetComponent<Renderer>().material = Green;
+        }
+        else if(!isClicked || !isHovered)
+        {
+            this.gameObject.GetComponent<Renderer>().material = Red;
+        }
+		
+	}
+
+    private void OnMouseEnter()
+    {
+        isHovered = true;
+    }
+
+
+    private void OnMouseExit()
+    {
+        isHovered = false;
+    }
+
+    private void OnMouseDown()
+    {
+        isClicked = true;
+        if (rtsMover.ActiveUnit != null)
+        {
+            rtsMover.ActiveUnit.GetComponent<UnitHighlight>().isClicked = false;
+        }
+        rtsMover.ActiveUnit = this.gameObject;
+    }
+}
