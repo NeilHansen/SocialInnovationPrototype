1d936ca3fe504c8f9d81d5477818a709fbaf65db
Hunter Fraser <hunterfraser.gamedev@gmail.com>
2019-02-02 21:52:19 -0500

    Ui , visual feedback and

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,7 +9,7 @@ public class GameManager : MonoBehaviour {
     public Text timerText;
     public Text timerTextBG;
     public float timeValue;
-    
+
     public Slider satisfactionMeter;
     public bool hasCustomer = false;
     public float defautlSatisfactionLevel;
@@ -18,7 +18,27 @@ public class GameManager : MonoBehaviour {
     //public float satisfactionLevel;
     public Text scoreText;
     private float currentFoodValue;
-    public float playerScore; 
+    public float playerScore;
+
+    //satisfaction visual Mods
+    [SerializeField]
+    Color[] Colors;
+    [SerializeField]
+    Sprite[] Satisfactionemojis;
+    [SerializeField]
+    Image SatisfactionFace;
+    [SerializeField]
+    Text SatisfactionText;
+    [SerializeField]
+    Text ScoreAdded;
+    //endScreen
+    [SerializeField]
+    GameObject EndingScreen;
+
+
+
+
+
 
 
 
@@ -28,6 +48,7 @@ public class GameManager : MonoBehaviour {
         timerTextBG.text = "" + (int)timeValue;
         satisfactionMeter.maxValue = defautlSatisfactionLevel;
         //StartNewCustomer();
+       
     }
 
 
@@ -42,12 +63,42 @@ public class GameManager : MonoBehaviour {
         if (timeValue <= 0)
         {
             Time.timeScale = 0.0f;
+            EndingScreen.SetActive(true);
+            EndingScreen.GetComponent<EndScreen>().EndGame();
+            
         }
 
         if (hasCustomer)
         {
             satisfactionMeter.value -= Time.deltaTime * 0.1f;
+            if (satisfactionMeter.value > 1.04f)
+            {
+                satisfactionMeter.GetComponentInChildren<Image>().color = Colors[0];
+                SatisfactionFace.sprite = Satisfactionemojis[0];
+                SatisfactionText.color = Colors[0];
+
+            }
+
+            else if (satisfactionMeter.value > .38f)
+            {
+                satisfactionMeter.GetComponentInChildren<Image>().color = Colors[1];
+                SatisfactionFace.sprite = Satisfactionemojis[1];
+                SatisfactionText.color = Colors[1];
+
+            }
+
+            else
+            {
+                satisfactionMeter.GetComponentInChildren<Image>().color = Colors[2];
+                SatisfactionFace.sprite = Satisfactionemojis[2];
+                SatisfactionText.color = Colors[2];
+
+            }
+
         }
+
+
+
     }
 
     public void Test()
@@ -63,9 +114,56 @@ public class GameManager : MonoBehaviour {
 
     public void AddScore()
     {
-        currentFoodValue = defaultFoodValue * satisfactionMeter.value;
+        //currentFoodValue = defaultFoodValue * satisfactionMeter.value;
+
+        if (satisfactionMeter.value > 1.04f)
+        {
+            currentFoodValue = 100;
+            StartCoroutine(FlashScoreAdded(0));
+        }
+
+        else if (satisfactionMeter.value > .38f)
+        {
+            currentFoodValue = 50;
+            StartCoroutine(FlashScoreAdded(1));
+        }
+        else
+        {
+            currentFoodValue = 25;
+            StartCoroutine(FlashScoreAdded(2));
+        }
+
+
+
+        ScoreAdded.text = "+" + currentFoodValue;
         playerScore += currentFoodValue;
         scoreText.text = "Score: " + (int)playerScore;
     }
 
+
+
+
+
+
+    //Flashes the amount of points the player received
+
+
+
+    IEnumerator FlashScoreAdded(int index)
+    {
+        Debug.Log("COROUTINE");
+        ScoreAdded.enabled = true;
+        ScoreAdded.color = Colors[index];
+        yield return new WaitForSeconds(1);
+        ScoreAdded.enabled = false;
+        StopCoroutine("FlashScoreAdded");
+    }
+
+
+
+
+
 }
+
+
+
