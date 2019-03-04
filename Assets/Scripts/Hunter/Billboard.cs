af1e169f39eb3936e73b60aeb4bb453e26af05f1
Hunter Fraser <hunterfraser.gamedev@gmail.com>
2019-03-03 23:31:03 -0500

    UI changes

---
+++ Assets/Scripts/Hunter/Billboard.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+
+    Camera RefCam;
+    public enum Axis { up,down,left,right,forward,back};
+    public bool reverseFace = false;
+    public Axis axis = Axis.up;
+
+    public Vector3 Getaxis (Axis refaxis)
+    {
+        switch (refaxis)
+        {
+            case Axis.up:
+                return Vector3.up;
+                
+            case Axis.down:
+                return Vector3.down;
+                
+            case Axis.left:
+                return Vector3.left;
+                
+            case Axis.right:
+                return Vector3.right;
+                
+            case Axis.forward:
+                return Vector3.forward;
+                
+            case Axis.back:
+                return Vector3.back;
+                
+        }
+
+        return Vector3.up;
+    }
+
+
+    private void Awake()
+    {
+        if (!RefCam)
+        {
+            RefCam = Camera.main;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+
+        Vector3 TargetPos = transform.position + RefCam.transform.rotation * (reverseFace ? Vector3.forward : Vector3.back);
+        Vector3 TargetOrientation = RefCam.transform.rotation * Getaxis(axis);
+        transform.LookAt(TargetPos, TargetOrientation);
+         
+		
+	}
+}
---
+++ Assets/Scripts/Hunter/ConstructionUI.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ConstructionUI : MonoBehaviour {
+
+    [SerializeField]
+    Image House;
+    [SerializeField]
+    Sprite[] HouseImages;
+    [SerializeField]
+    TextMeshProUGUI ProgressText;
+   
+
+
+
+
+    // Use this for initialization
+    void Start () {
+        AddToHouse(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    public void AddToHouse(int Score)
+    {
+
+        switch (Score)
+        {
+
+            case 0:
+                House.enabled=false;
+                ProgressText.text = "Progress: " + Score + "/4";
+                break;
+
+
+            case 1:
+                House.enabled = true;
+                House.sprite = HouseImages[0];
+                ProgressText.text = "Progress: " + Score + "/4";
+
+                break;
+
+            case 2:
+                House.enabled = true;
+                House.sprite = HouseImages[1];
+                ProgressText.text = "Progress: " + Score + "/4";
+
+                break;
+
+            case 3:
+                House.enabled = true;
+                House.sprite = HouseImages[2];
+                ProgressText.text = "Progress: " + Score + "/4";
+                break;
+
+            case 4:
+                House.enabled = true;
+                House.sprite = HouseImages[3];
+                ProgressText.text = "Progress: " + Score + "/4";
+                break;
+
+        }
+
+    }
+}
