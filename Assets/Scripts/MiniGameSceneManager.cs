10c23aed1933b86317d98af70339ff6246988cc0
Neil Hansen <neilmickneilh12@hotmail.com>
2019-03-17 17:38:19 -0400

    added placeholder hover icons to cooks in the kitchen scene, added simple menu scene and functionality

---
+++ Assets/Scripts/MiniGameSceneManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MiniGameSceneManager : MonoBehaviour {
+    //name of mini game
+
+    
+
+    //load that index
+    public void LoadScene(string levelToLoad)
+    {
+      SceneManager.LoadScene(levelToLoad);
+    }
+}
