e2f06778fd7594db1d86b4bd8118c3f12b5b7419
Neil Hansen <neilmickneilh12@hotmail.com>
2019-08-06 21:08:19 -0400

    added blackboard loader and player prefs editor scripts

---
+++ Assets/Scripts/Blackboard/BlackboardLoader.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+using System;
+
+
+[System.Serializable]
+public class TokenClassName
+{
+    public string access_token;
+}
+
+public class BlackboardLoader : MonoBehaviour {
+
+    public static string url = "https://bbgbctest.blackboard.com/learn/api/public/v1/oauth2/token" ;
+
+    // Use this for initialization
+    void Start () {
+        DoThing();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+   
+    public void DoThing()
+    {
+        StartCoroutine(GetAccessToken(url));
+    }
+
+    private static IEnumerator GetAccessToken(string token)
+    {
+        WWWForm form = new WWWForm();
+
+        form.AddField("redirect_uri", "uri");
+        form.AddField("response_type", "login-secret");
+        form.AddField("client_id", "id");
+        //Fill key and value
+
+
+        ///UnityWebRequest www = UnityWebRequest.Get(BlackboardLoader.url, form);
+        WWW www = new WWW(url, form);
+
+        //Send request
+        yield return www.isDone;
+
+        if (String.IsNullOrEmpty(www.error))
+        {
+            string resultContent = www.text;
+            Debug.Log(resultContent);
+            TokenClassName json = JsonUtility.FromJson<TokenClassName>(resultContent);
+
+            //Return result
+            Debug.Log("Token: " + json.access_token);
+        }
+        else
+        {
+            //Return null
+            UnityEngine.Debug.Log("WWW Error: " + www.error);
+        }
+    }
+
+
+}
