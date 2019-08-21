4a8545ae7b0ca411b72799d444d4a6f6ac23a220
NeilHansen <neil.hansen17@gmail.com>
2019-08-20 20:42:20 -0400

    got a token from our local server

--- Assets/Scripts/Blackboard/API.cs
+++ Assets/Scripts/Blackboard/API.cs
@@ -29,27 +29,36 @@ public class API : MonoBehaviour {
     {
         WWWForm form = new WWWForm();
         form.AddField("grant_type", "client_credentials");
-
-
-        //headers["grant_type"] = "client_credentials";
-
-        //form.AddField("grant_type", "ce2cbd52-60e4-4e94-bb23-66099afe6d16:ANuwg76VOV7aIA0KYqfv6KWIGOWEw0FX");
-        //form.AddField("Authorization", "ANuwg76VOV7aIA0KYqfv6KWIGOWEw0FX");
-       
-      //  WWW request = new WWW(BlackboardEndpoint,form.data, headers);
        
         UnityWebRequest request = UnityWebRequest.Post(BlackboardEndpoint, form);
         request.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         
-        string blah = "ce2cbd52-60e4-4e94-bb23-66099afe6d16:ANuwg76VOV7aIA0KYqfv6KWIGOWEw0FX";
-        // blah =  System.Text.Encoding.UTF8.GetBytes(blah);
-        request.SetRequestHeader("Authorization","Basic "  + EncodeTo64(blah));
-        //request.SetRequestHeader("grant_type", "client_credentials");
+        string OAuthURI = "ce2cbd52-60e4-4e94-bb23-66099afe6d16:ANuwg76VOV7aIA0KYqfv6KWIGOWEw0FX";
+        
+        request.SetRequestHeader("Authorization","Basic "  + EncodeTo64(OAuthURI));
+     
         StartCoroutine(OnResponse(request));
     }
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("http://127.0.0.1:8000/");
+        yield return www.SendWebRequest();
 
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
 
-   
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+
+            responseText.text = "Token: " + www.downloadHandler.text + "Error Code: " + www.error;
+        }
+    }
 
     private IEnumerator OnResponse(UnityWebRequest req)
     {
@@ -68,37 +77,11 @@ public class API : MonoBehaviour {
         Debug.Log("Token: " + req.downloadHandler.text + "Error Code: " + req.error);
         string temp = req.downloadHandler.text;
         OAuth2Token json = JsonUtility.FromJson<OAuth2Token>(temp);
-        // { "access_token":"tE1RgRvCI12WpQEa6mZbOiAO3954Sz22","token_type":"bearer","expires_in":2582}
+        
         Debug.LogError(json.access_token);
-        //StartCoroutine(rest(json.access_token));
-
     }
 
-    public IEnumerator rest(string token)
-    {
-        WWWForm form = new WWWForm();
-        form.AddField("input", test);
-
-        UnityWebRequest request = UnityWebRequest.Post("https://bbgbctest.blackboard.com/learn/api/public/v1/announcements", form);
-        request.SetRequestHeader("Content-Type", "application/json");
-
-        // blah =  System.Text.Encoding.UTF8.GetBytes(blah);
-        request.SetRequestHeader("Authorization", "Bearer " + token);
-        //  request.SetRequestHeader("grant_type", "client_credentials");
-
-        yield return request.SendWebRequest();
-        if (request.isNetworkError)
-        {
-            Debug.Log("Error While Sending: " + request.error);
-        }
-        else
-        {
-            Debug.Log("Received: " + request.downloadHandler.text);
-        }
-
-
-
-    }
+   
     static public string EncodeTo64(string toEncode)
     {
         byte[] toEncodeAsBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(toEncode);
@@ -119,7 +102,31 @@ public class API : MonoBehaviour {
 
     private void Start()
     {
-        Request();
+       // Request();
+        StartCoroutine(GetText());
     }
+
+    //public IEnumerator rest(string token)
+    //{
+    //    WWWForm form = new WWWForm();
+    //    form.AddField("input", test);
+
+    //    UnityWebRequest request = UnityWebRequest.Post("https://bbgbctest.blackboard.com/learn/api/public/v1/announcements", form);
+    //    request.SetRequestHeader("Content-Type", "application/json");
+
+    //    // blah =  System.Text.Encoding.UTF8.GetBytes(blah);
+    //    request.SetRequestHeader("Authorization", "Bearer " + token);
+    //    //  request.SetRequestHeader("grant_type", "client_credentials");
+
+    //    yield return request.SendWebRequest();
+    //    if (request.isNetworkError)
+    //    {
+    //        Debug.Log("Error While Sending: " + request.error);
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("Received: " + request.downloadHandler.text);
+    //    }
+    //}
 
 }
